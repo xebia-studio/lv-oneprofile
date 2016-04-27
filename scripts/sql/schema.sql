d0a1000c603bd34f01c96cebf9dc65e606e9cf5f
asudre <asudre@xebia.fr>
2016-04-27 15:10:58 +0200

    [Database script] SQL script generating the insert request for users from LiVe structure

--- scripts/sql/schema.sql
+++ scripts/sql/schema.sql
@@ -12,3 +12,29 @@ CREATE TABLE users (
   windows_account TEXT,
   uniqueID        TEXT
 );
+
+SELECT
+  CONCAT('INSERT INTO users (
+    "username",
+    "lastname",
+    "firstname",
+    "mail",
+    "country",
+    "zone",
+    "seniority",
+    "work_location")
+    values (''', infos.username, ''', ''', infos.lastname, ''', ''', infos.firstname, ''', ''', infos.mail, ''', ''', ifnull(infos.country, ''), ''', ''', ifnull(infos.zone, ''), ''', ''', ifnull(infos.seniority, ''), ''', ''', ifnull(infos.work_location, ''), ''')')
+  from (SELECT
+      id,
+      displayName as username,
+      SUBSTRING_INDEX(SUBSTRING_INDEX(displayName, ' ', 1), ' ', -1) AS firstname,
+      CONCAT(If(  length(displayName) - length(replace(displayName, ' ', ''))>1,
+		  CONCAT(SUBSTRING_INDEX(SUBSTRING_INDEX(displayName, ' ', 2), ' ', -1), ' '), ''),
+      SUBSTRING_INDEX(SUBSTRING_INDEX(displayName, ' ', 3), ' ', -1)) AS lastname,
+      email as mail,
+      country,
+      area as zone,
+      seniority,
+      store as work_location
+      FROM users
+  ) as infos;
