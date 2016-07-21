864222f8b31621f9cfcc5ca5bd7c1bb4f86477e7
asudre <asudre@xebia.fr>
2016-07-21 17:49:52 +0200

    [oneprofile] change update, only change mail to email

--- scripts/sql/diff/13072016_alter_users_table.sql
+++ scripts/sql/diff/13072016_alter_users_table.sql
@@ -1,23 +1,5 @@
 ALTER TABLE users
 RENAME COLUMN mail TO email;
 
-ALTER TABLE users
-ADD password TEXT;
-
-ALTER TABLE users
-ADD token TEXT;
-
-ALTER TABLE users
-ADD description TEXT;
-
-ALTER TABLE users
-ADD phone TEXT;
-
-ALTER TABLE users
-ADD resetToken TEXT;
-
-ALTER TABLE users
-ADD resetDemandExpirationDate TEXT;
-
 update users set seniority = concat(trim(to_char(to_timestamp(seniority, 'YYYY-MM-DD'), 'Month')), ' ', to_char(to_timestamp(seniority, 'YYYY-MM-DD'), 'YYYY'));
 
